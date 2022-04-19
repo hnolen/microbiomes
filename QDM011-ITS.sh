d08ebd222e1f45531ddb562ca9e00935d673e5f4
hnolen <57228092+hnolen@users.noreply.github.com>
2022-04-19 13:06:29 -0400

    cutadapt + itsxpress

--- QDM011-ITS.sh
+++ QDM011-ITS.sh
@@ -5,5 +5,9 @@
 ##importing reads from genome center
 qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' --input-path ./reads/ --input-format CasavaOneEightSingleLanePerSampleDirFmt --output-path ./imported_reads.qza
 
+##getting quality plots
+qiime demux summarize --i-data imported_reads.qza --o-visualization quality.qzv
+##trim forward at 227
 
-
+#running cutadapt and itsxpress
+sbatch itsXpress.slurm
